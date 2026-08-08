2b52a948589abff22273a32dc0df6bcda51eafe8
Anurag Katiyar <88178410+kanurag-dev@users.noreply.github.com>
2026-08-08 19:27:20 +0530

    Prepare backend for deployment

--- expense-tracker/backend/index.js
+++ expense-tracker/backend/index.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 const app=express()
 const connectDB=require("./db")
 const expenseROutes=require("./routes/expenses")
